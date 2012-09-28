d3c94bbbb328bc25feb4fc8f2e66603cd88f1f43
Roberto Weller <cs.utexas.edu>
2012-09-28 18:49:39 -0500

    final commit

--- Voting.h
+++ Voting.h
@@ -1,8 +1,8 @@
 
 /*
 To run the program:
-    % g++ -pedantic -std=c++0x -Wall RunVoting.c++ -o RunVoting.c++.app
-    % valgrind RunVoting.c++.app < RunVoting.in >& RunVoting.out
+    % g++ -pedantic -std=c++0x -Wall RunVoting3.c++ -o RunVoting3.c++.app
+    % valgrind RunVoting3.c++.app < RunVoting.in >& RunVoting.out
 
 To configure Doxygen:
     % doxygen -g
@@ -28,156 +28,206 @@ To document the program:
 // --------
 // includes
 // --------
+#include <vector>
+#include <iostream> 
+#include <cstdlib>
+//#include "Voting.h"
+#include <cassert>  
+#include <iostream> 
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <stdio.h>
+#include <sstream>
+#include <iterator>
 
-#include <iostream> // cin, cout, ios_base
 
-#include "Voting.h"
-
-// ----
-// main
-// ----
-
-int main () {
-    using namespace std;
-    ios_base::sync_with_stdio(false); // turn off synchronization with C I/O
-    voting_solve(cin, cout);
-    return 0;}
-
-// --------
-// includes
-// --------
-
-#include <cassert>  // assert
-#include <iostream> // endl, istream, ostream
-
+//has functions
+//
+void voting_eval (std::istream& r, std::ostream& w);
+int  checkWinner(std::vector<std::vector<std::vector<int> > >& voteList, int candidates, int voteCounter, std::vector<std::string> candidateList);
+int  checkTie(std::vector<std::vector<std::vector<int> > >& voteList, int candidates, int voteCounter,std::vector<std::string> candidateList);
+void checkLosers (std::vector<std::vector<std::vector<int> > >& voteList, int voteCounter, int candidates, int& loserCounter, std::vector<int>& loserList);
+void revote(std::vector<std::vector<std::vector<int> > >& voteList, int voteCounter, int& candidates, int& loserCounter, std::vector<int>& loserList);
 
 // ------------
 // voting_eval
 // ------------
-void voting_solve (std::istream& r, std::ostream& w,){
+void voting_eval (std::istream& r, std::ostream& w)
+{
+    using namespace std;
+    //get number of candidates
     int candidates;
-    char line[];
-    getline(r,line,char delim);
-    candidates = atoi(line);
-    std::vector<string> candidateList;
-    
+    string newLine;
+    getline(r,newLine);
+    candidates = atoi(newLine.c_str());
+    std::vector<std::string> candidateList;
     //fill list of candidates
-    
-    for (int i=0; i<candidates; i++) {
-        getline(r,line,char delim);
-        candidateList.push_back(line);
+    string newestLine;
+    for (int i=0; i<candidates; i++)
+    {
+        getline(r,newestLine);
+        candidateList.push_back(newestLine);
     }
-    
     //read and fill first round of votes
-    std::vector<std::vector<std::vector<int>>> voteList;
+    std::vector<std::vector<std::vector<int> > > voteList;
+    unsigned int newSize = (unsigned int)candidates;
+    voteList.resize(newSize);
     std::vector<int> vote;
     int whiteLine = 0;
-    int voteCounter;
-    while (whiteLine == 0) {
-        char tempVote [candidates];
-        getline(r,tempVote,char delim);
-        if (strcmp(tempVote,"")==0) {
+    int voteCounter = 0; 
+    while (whiteLine == 0)
+    {
+        string tempVote;
+        getline(r,tempVote);
+        if (tempVote.size() == 0)
+        {
             whiteLine = 1;
         }
-        else{
+        else
+        {
             int rank;
-            for (int j=0; j<candidates; j++) {
-                rank = atoi(tempVote[j]);
-                vote.push_back(rank);
+            istringstream iss(tempVote);
+            vector<string> tokens;
+            copy(istream_iterator<string>(iss),
+                 istream_iterator<string>(),
+                 back_inserter<vector<string> >(tokens));
+
+            for (int k=0; k < (int) tokens.size(); k++)
+            {
+		rank = atoi(tokens[k].c_str());
+		vote.push_back(rank);
             }
-            int place = vote.front();
+            int place = vote.front()-1;
             vote.erase(vote.begin());
+            assert(place >= 0);
+            assert(place < (int)voteList.size());
             voteList[place].push_back(vote);
             voteCounter++;
+            vote.clear();
         }
-    }
-    int isTied = 0;
-    // loop while there is no winner or tie
-    while (checkWinner(voteList,candidates,voteCounter,candidateList)<0 || checkTie(voteList,candidates,voteCounter,candidateList)< 0 {
-        if (check_tied() != 0) {
-            isTied = 1
-        }
-        else{
-            int loserCounter;
-            std::vector<int> loserList;
-            checkLosers(voteList,voteCounter,candidates,loserCounter,loserList);
-            revote();
-        }
+    } 
+    int loserCounter = 0;
+    std::vector<int> loserList;
+    
+    while (!(checkWinner(voteList,candidates,voteCounter,candidateList)==0) && !(checkTie(voteList,candidates,voteCounter, candidateList)== 0))
+    {
+       
+      
+
+        checkLosers(voteList,voteCounter,candidates,loserCounter,loserList);
+        revote(voteList, voteCounter, candidates, loserCounter, loserList);
         
         
+        
     }
-    if(isTied == 1){
-        printTiedCandidates   
-    }
-    else
-        printWinner
 
 }
 // -------------
 // checkWinner
 // -------------
 
-int checkWinner(std::vector<std::vector<std::vector<int>>>& voteList, int candidates, int voteCounter,vector<string> candidateList){
+int checkWinner(std::vector<std::vector<std::vector<int> > >& voteList, int candidates, int voteCounter, std::vector<std::string> candidateList)
+{
+    using namespace std;
+   
     int votesNeeded;
-    if (voteCounter % 2 == 0) {
-        votesNeeded = voteCounter/2;
+    if ((voteCounter % 2) == 0)
+    {
+        
+        votesNeeded = voteCounter/2 + 1;
     }
-    else{
+    else
+    {
         votesNeeded = (voteCounter+1)/2;
     }
+    
     int votes;
-    for (int i=0; i<candidates; i++) {
+    
+    for (int i=0; i<candidates; i++)
+    {
         votes = voteList[i].size();
-        if (votes>= votesNeeded) {
-            for (int j = 0; j < candidateList[i].length(); j++) {
+	
+        if (votes>= votesNeeded)
+        {
+            for (int j = 0; j < (int)candidateList[i].length(); j++)
+            {
                 cout << candidateList[i][j];
             }
             cout << endl;
             return 0;
         }
-        else{
+       
+    }
+    return -1;
+}
+// -------------
+// checkTie
+// -------------
+           
+int checkTie(std::vector<std::vector<std::vector<int> > >& voteList, int candidates, int voteCounter,std::vector<std::string> candidateList)
+{
+    using namespace std;
+   
+    int isTie = 1;
+    int tiedVoteNumber;
+    for (int n = 0; n < candidates; n++)
+    {
+    if (voteList[n].size()>0)
+    {
+        tiedVoteNumber = voteList[n].size();
+    }
+    }
+
+    for (int i = 0; i<candidates; i++)
+    {
+        
+        if ((int)voteList[i].size()!=tiedVoteNumber && (int)voteList[i].size() > 0)
+        {
+           
+            isTie = 0;
             return -1;
         }
     }
-}
-// -------------
-// checkWinner
-// -------------
-           
-int checkTie(std::vector<std::vector<std::vector<int>>>& voteList, int candidates, int voteCounter,std::vector<string> candidateList){
-    int isTie = 1;
-    int tiedVoteNumber = voteList[0].size();
-    for (int i = 0; i<candidates; i++) {
-        if (voteList[i].size()!=tiedVoteNumber) {
-            isTie = 0;
-        }
+   
+    for (int j = 0; j < candidates; j++)
+    {
+         if((int)voteList[j].size() > 0)
+         {
+                cout << candidateList[j] << endl;
+         }
+        
     }
-    if (isTie) {
-        for (int j; j<candidates; j++) {
-            for (int k = 0; k < candidateList[j].length(); j++) {
-                cout << candidateList[j][k];
-            }
-            cout << endl;
-        }
-        return 0;
-    }
-    else
-        return -1;
+    return 0;
+    
+    
     
 }
 // -------------
 // checkLosers
 // -------------
-void checkLosers (std::vector<std::vector<std::vector<int>>>& voteList, int voteCounter, int candidates, int& loserCounter, std::vector<int>& loserList){
+void checkLosers (std::vector<std::vector<std::vector<int> > >& voteList, int voteCounter, int candidates, int& loserCounter, std::vector<int>& loserList)
+{    
+    using namespace std;
+    
     int minVote = 1000;
-    for (int i=0; i<=candidates; i++) {
-        if (voteList[i].size() <= minVote) {
+    for (int i=0; i<candidates; i++)
+    {
+	
+        if ((int)voteList[i].size() <= minVote && (int)voteList[i].size() > 0)
+        {
+
             minVote = voteList[i].size();
         }
+	
     }
-    for (int j=0; j<candidates; j++) {
-        if (voteList[j].size() == minVote) {
-            loserList.push_back(j)
+    for (int j=0; j<candidates; j++)
+    {
+	
+        if ((int)voteList[j].size() == minVote && (int)voteList[j].size() > 0)
+        {
+	
+            loserList.push_back(j);
             loserCounter++;
         }
     }
@@ -185,66 +235,50 @@ void checkLosers (std::vector<std::vector<std::vector<int>>>& voteList, int vote
 // -------------
 // revote
 // -------------
-void revote(std::vector<std::vector<std::vector<int>>>& voteList, int voteCounter, int& candidates, int& loserCounter, std::vector<int>& loserList){
-    loserList.push_back(-1);
-    for (i = 0; i< loserCounter; i++) {
-        int currentLoser = LoserList[i];
-        for (int j = 0; j< voteList[currentLoser].size(); j++) {
+void revote(std::vector<std::vector<std::vector<int> > >& voteList, int voteCounter, int& candidates, int& loserCounter, std::vector<int>& loserList)
+{
+    using namespace std;
+   
+    // go through all losers in loserList
+    for (int i = 0; i< loserCounter; i++)
+    {
+        int currentLoser = loserList[i];
+        // go through all votes of current loser
+        for (int j = 0; j< (int)voteList[currentLoser].size(); j++)
+        {
+            //make sure the first rank of vote is not for another loser
             int validRank = 0;
-            int firstRank = voteList[currentLoser].front().front();
-            while (validRank ==0) {
-                if (find(loserList.begin(),loserList.end(),firstRank) == -1) {
+            int firstRank = voteList[currentLoser].front().front() -1;
+            while (validRank ==0)
+            {
+                int returnVal = 1;
+                for (int p = 0; p < loserCounter; p++)
+                {
+                    int tempVal = loserList[p];
+                    if (firstRank == tempVal)
+                    {
+                        returnVal = 0;
+                    }
+                }
+                if (returnVal == 1)
+                {
                     validRank = 1;
                 }
-                else{
-                    voteList[currentLoser].front().erase(voteList[currentLoser].front().begin()];
-                    firstRank = voteList[currentLoser].front().front();
+                else
+                {
+                    //erase the first rank of vote
+                    voteList[currentLoser].front().erase(voteList[currentLoser].front().begin());
+                    //update first rank
+                    firstRank = voteList[currentLoser].front().front()-1;
                 }
             }
+            // erases first rank of vote being treated
             voteList[currentLoser].front().erase(voteList[currentLoser].front().begin());
-            voteList[firstRank].push_back(voteList[CurrentLoser].front());
-            voteList[currentLoser].erase(voteList[currentLolser].begin();
+            // adds vote to firstRank
+            voteList[firstRank].push_back(voteList[currentLoser].front());
+            // erases vote from currentLoser
+            voteList[currentLoser].erase(voteList[currentLoser].begin());
             
         }
-        voteList.erase(voteList.begin()+currentLoser-1);
-        candidates--;
     }
 }
-
-// -------------
-// voting_print
-// -------------
-
-                                                             
-/**
- * prints the values of i, j, and v
- * @param w a std::ostream
- * @param i the string with name of winner
- */
-void voting_print (std::ostream& w, int i, int j, int v) {
-    assert(strcmp(i,"")!=0);
-    w << i << std::endl;}
-
-// -------------
-// voting_solve
-// -------------
-
-/**
- * @param r a std::istream
- * @param w a std::ostream
- */
-void voting_solve (std::istream& r, std::ostream& w) {
-    int testNumber;
-    char line[];
-    getline(r,line,char delim);
-    testNumber = atoi(line);
-    char winner[];
-    int i;
-    getline(r,line,char delim);
-    for (int i=0; i<testNumber; i++) {
-        voting_eval(r,w);
-    }
-
-
-
-
